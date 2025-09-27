18f88ef4836ebcce28ba817621f5e8252264c99d
Darrell <darrelltstevens@gmail.com>
2025-09-27 15:57:51 +0000

    Auto commit before resetting main branch

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import App from './App.tsx'
 import { ErrorFallback } from './ErrorFallback.tsx'
 
 import "./main.css"
+import "./styles/theme.css"
 import "./index.css"
 
 createRoot(document.getElementById('root')!).render(
